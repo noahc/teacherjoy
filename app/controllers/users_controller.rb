74e4468681c8a3b3db0b8d1b745b7bc582cbcde8
Noah Clark <noah@noahc.net>
2012-07-16 13:57:42 -0500

    change user edit redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
-      redirect_to @user
+      redirect_to holders_path
     else
       render 'edit'
     end
